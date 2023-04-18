4bf09e6171613b644d41313113314e3dc0cf8f2e
ashton8504 <ashtonlittle8504@gmail.com>
2023-04-18 11:48:01 -0400

    saving work

--- script.js
+++ script.js
@@ -5,59 +5,6 @@ button.addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
 
-// function addFavorite() {
-//   let urlInput = document.getElementById("typeURL").value;
-//   let li = document.createElement("li");
-//   let a = document.createElement("a");
-//   let deleteButton = document.createElement("button");
-//   let img = document.createElement("img");
-
-//   a.href = urlInput;
-//   a.target = "_blank";
-//   let faviconUrl =
-//     "https://www.google.com/s2/favicons?sz=64&domain=" + urlInput;
-//   img.src = faviconUrl;
-//   img.onerror = function () {
-//     this.onerror = null;
-//     this.src = "https://via.placeholder.com/64x64?text=No+Icon";
-//   };
-//   img.style.borderRadius = "50%";
-//   img.style.width = "64px";
-//   img.style.height = "64px";
-//   a.appendChild(img);
-//   li.classList.add(
-//     "list-group-item",
-//     "list-group-item-action",
-//     "d-flex",
-//     "flex-column",
-//     "align-items-center"
-//   );
-//   li.style.padding = "0.75rem 1.25rem";
-//   li.appendChild(a);
-
-//   deleteButton.classList.add(
-//     "btn",
-//     "btn-danger",
-//     "btn-sm",
-//     "delete-button",
-//     "mt-2"
-//   );
-//   deleteButton.innerHTML = "Delete";
-//   deleteButton.addEventListener("click", function () {
-//     li.remove();
-//   });
-//   li.appendChild(deleteButton);
-
-//   document.getElementById("favoriteList").appendChild(li);
-
-//   const favorites = getFavorites();
-//   const newFavorite = { url: urlInput };
-//   favorites.push(newFavorite);
-//   saveFavorites(favorites);
-
-//   document.getElementById("typeURL").value = "";
-// }
-
 function addFavorite() {
   let urlInput = document.getElementById("typeURL").value;
   let li = document.createElement("li");
