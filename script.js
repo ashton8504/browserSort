82a8bcc73b6c4b85c3638533c04d0b26cbc20419
ashton8504 <ashtonlittle8504@gmail.com>
2023-04-18 09:48:31 -0400

    saving basic layout

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle Dark/Light mode
+
+const button = document.getElementById("mode-toggle");
+button.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
