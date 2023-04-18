91c6489b5012751d8dd962061435e6f70255b4ba
ashton8504 <ashtonlittle8504@gmail.com>
2023-04-18 10:55:47 -0400

    saving progress

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ const button = document.getElementById("mode-toggle");
 button.addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
+
+function addFavorite() {
+  // let urlInput = document.getElementById("typeURL").value;
+  // let li = document.createElement("li");
+  // let a = document.createElement("a");
+
+  // a.href = urlInput;
+  // a.target = "_blank";
+  // let faviconUrl =
+  //   "https://www.google.com/s2/favicons?sz=64&domain=" + urlInput;
+  // a.innerHTML =
+  //   '<img src="' +
+  //   faviconUrl +
+  //   '" onerror="this.onerror=null; this.src=\'https://via.placeholder.com/64x64?text=No+Icon\';" class="mr-2">' +
+  //   urlInput;
+  // li.classList.add("list-group-item", "list-group-item-action");
+  // li.appendChild(a);
+
+  // document.getElementById("favoriteList").appendChild(li);
+
+  let urlInput = document.getElementById("typeURL").value;
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+
+  a.href = urlInput;
+  a.target = "_blank";
+  let faviconUrl =
+    "https://www.google.com/s2/favicons?sz=64&domain=" + urlInput;
+  a.innerHTML =
+    '<img src="' +
+    faviconUrl +
+    '" onerror="this.onerror=null; this.src=\'https://via.placeholder.com/64x64?text=No+Icon\';" class="mr-2">';
+  li.classList.add("list-group-item", "list-group-item-action");
+  li.appendChild(a);
+
+  document.getElementById("favoriteList").appendChild(li);
+}
